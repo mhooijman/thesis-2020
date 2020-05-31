78d7f83ac9cc8b5722e94c3924bc18f2b6545fd7
mhooijman <macbook@macs-mbp.home>
2020-05-31 08:19:18 +0200

    test

--- encoding_experiment.py
+++ encoding_experiment.py
@@ -25,7 +25,7 @@ def do_gender_encoding_experiment(sets, activations_dir, speakers_data):
     for set in sets:
         set_path = '{}/{}'.format(activations_dir, set['set_id'])
         activation_files = os.listdir(set_path)
-        for file_name in [f for f in activation_files if f.endswith('.npy'):
+        for file_name in [f for f in activation_files if f.endswith('.npy')]:
             file_path = '{}/{}'.format(set_path, file_name)
             print(file_path)
             data.append(np.load(open(file_path)))
