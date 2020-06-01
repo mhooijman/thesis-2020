8f74bd0a60d43d337357ce3ae788e50aeec8ad67
mhooijman <martijnhooijman@gmail.com>
2020-06-01 19:50:58 +0200

    tmp

--- encoding_experiment.py
+++ encoding_experiment.py
@@ -187,7 +187,7 @@ def do_sentence_encoding_experiment_libri_speech(activations_dir, sentence_data)
     for label in set(labels):
         counter[label] = labels.count(label)
 
-    print(dict(sorted(counter.items(), key=lambda kv: kv[1], reverse=True)))
+    print(dict(sorted(counter.items(), key=lambda kv: kv[1], reverse=False)))
 
     import sys
     sys.exit(1)
