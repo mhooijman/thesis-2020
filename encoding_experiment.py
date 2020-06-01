6ca4b4b456d25dd077210be4fff3cd540c206388
mhooijman <martijnhooijman@gmail.com>
2020-06-01 19:06:03 +0200

    tmp

--- encoding_experiment.py
+++ encoding_experiment.py
@@ -238,10 +238,10 @@ def main():
     # results_pruned_model_libri = do_gender_encoding_experiment_libri_speech(
     #                 activations_dir=activations_dir, speaker_data=speaker_data_librispeech)
 
-    # # Encoding experiment of gender on 0.1 pruned model activations of librispeech
-    # activations_dir = './results/activations/libri/pruned-10.0-random'
-    # results_random_pruned_model_libri = do_gender_encoding_experiment_libri_speech(
-    #                 activations_dir=activations_dir, speaker_data=speaker_data_librispeech)
+    # Encoding experiment of gender on 0.1 pruned model activations of librispeech
+    activations_dir = './results/activations/libri/pruned-10.0-random'
+    results_random_pruned_model_libri = do_gender_encoding_experiment_libri_speech(
+                    activations_dir=activations_dir, speaker_data=speaker_data_librispeech)
 
     # total_results = {
     #     'common_voice': {
@@ -277,36 +277,36 @@ def main():
     # results_random_pruned_model_common = do_sentence_length_encoding_experiment_common_voice(sets=sets_to_use, 
     #                 activations_dir=activations_dir)
 
-    # Encoding experiment of gender on full model activations of librispeech
-    activations_dir = './results/activations/libri'
-    results_full_model_libri = do_sentence_encoding_experiment_libri_speech( 
-                    activations_dir=activations_dir, sentence_data=sentence_data_librispeech)
+    # # Encoding experiment of gender on full model activations of librispeech
+    # activations_dir = './results/activations/libri'
+    # results_full_model_libri = do_sentence_encoding_experiment_libri_speech( 
+    #                 activations_dir=activations_dir, sentence_data=sentence_data_librispeech)
 
-    # Encoding experiment of gender on 0.1 pruned model activations of librispeech
-    activations_dir = './results/activations/libri/pruned-10.0'
-    results_pruned_model_libri = do_sentence_encoding_experiment_libri_speech( 
-                    activations_dir=activations_dir, sentence_data=sentence_data_librispeech)
+    # # Encoding experiment of gender on 0.1 pruned model activations of librispeech
+    # activations_dir = './results/activations/libri/pruned-10.0'
+    # results_pruned_model_libri = do_sentence_encoding_experiment_libri_speech( 
+    #                 activations_dir=activations_dir, sentence_data=sentence_data_librispeech)
 
-    # Encoding experiment of gender on 0.1 pruned model activations of librispeech
-    activations_dir = './results/activations/libri/pruned-10.0-random'
-    results_random_pruned_model_libri = do_sentence_encoding_experiment_libri_speech( 
-                    activations_dir=activations_dir, sentence_data=sentence_data_librispeech)
+    # # Encoding experiment of gender on 0.1 pruned model activations of librispeech
+    # activations_dir = './results/activations/libri/pruned-10.0-random'
+    # results_random_pruned_model_libri = do_sentence_encoding_experiment_libri_speech( 
+    #                 activations_dir=activations_dir, sentence_data=sentence_data_librispeech)
 
-    total_results = {
+    # total_results = {
         # 'common_voice': {
         #     'full': results_full_model_common, 
         #     'imp-score-10': results_pruned_model_common,
         #     'random-10': results_random_pruned_model_common
         # }
         # ,
-        'libri_speech': {
-            'full': results_full_model_libri,
-            'imp-score-10': results_pruned_model_libri,
-            'random-10': results_random_pruned_model_libri
-        }
-    }
+    #     'libri_speech': {
+    #         'full': results_full_model_libri,
+    #         'imp-score-10': results_pruned_model_libri,
+    #         'random-10': results_random_pruned_model_libri
+    #     }
+    # }
     
-    json.dump(total_results, open('./results/sentence_length_encoding_experiment_results.json', 'w+'))
+    # json.dump(total_results, open('./results/sentence_length_encoding_experiment_results.json', 'w+'))
 
 
 if __name__ == "__main__":
